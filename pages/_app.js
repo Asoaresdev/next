8d9494a83a8e288660c83dd6662595ca73575699
Andrea Soares <asoaresdev@gmail.com>
2023-09-19 16:57:19 -0300

    Feat: css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "../styles/globals.css"
-
+//Estilo global só pode ser importado no _app
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />;
   }
